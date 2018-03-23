6d08e7ac124d502c3d24e6e4861fb30af9aae579
Jeff Becker <jeff@i2p.rocks>
2018-03-23 08:52:51 -0400

    more

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,10 @@
 package server
 
+import (
+	"path/filepath"
+)
+
 func (s *Server) SetupAppRoutes() {
 	s.e.GET("/", s.ServeIndex)
-	s.e.Static("/static/", s.AssetsRoot)
+	s.e.Static("/static/", filepath.Join(s.AssetsRoot, "static"))
 }
